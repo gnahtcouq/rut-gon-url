7896e695a10f952a413cdf6699fac0507bb4ffd1
Quoc Thang <comehere.thang@gmail.com>
2019-06-09 14:04:57 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?
+	if(isset($_GET['k'])){
+		// Khai báo mảng
+		$data = array();
+
+		// Lấy dữ liệu hiện tại (nếu có)
+		if(file_exists('data.txt')){
+			$json = file_get_contents('data.txt');
+			$data = json_decode($json, true);
+		}
+
+		if(isset($data[$_GET['k']])){ // Có tồn tại link rút gọn
+			unset($data[$_GET['k']]);
+		}
+
+		// Ghi đè lại dữ liệu
+		file_put_contents('data.txt', json_encode($data));
+	}
+
+	header('location:https://quocthang.gq/');exit;
+?>
